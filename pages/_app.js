a67ebbcc6ea2756176af61c0fb90397684e4b187
Sanjay Patel <sanjay@pixer.io>
2022-11-03 22:12:09 +0530

    added basic SEO configuration and styled components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,74 @@
-import '../styles/globals.css'
+/* eslint-disable max-len */
+import "../styles/globals.css";
+import { DefaultSeo } from "next-seo";
+import { ThemeProvider } from "styled-components";
+import SEO from "../next-seo.config";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const theme = {
+  fontfamily: {
+    Bagoss: "Bagoss",
+    Everett: "Everett",
+  },
+  colors: {
+    primary: "#09AA6C",
+    title: "#131313",
+    body: "#4C4C4C",
+    lightgray: "#757575",
+    midiumgray: "#5B5B5B",
+    black: "#000000",
+    whiteColor: "#FFFFFF",
+    neutral: "#F3F3F2",
+    darkPrimary: "#2e69ce",
+    textColor: "#424242",
+    lightDark: "#333333",
+    greendark: "#00160E",
+    greenmiddark: "#003F27",
+    greenmidlight: "#7DDAA0",
+    greenlight: "#E3FFEE",
+    blueprimary: "#00AFB9",
+    bluedark: "#01292C",
+    bluemiddark: "#01292C",
+    bluemidlight: "#7ADCE1",
+    bluelight: "#E0FBFD",
+    purpleprimary: "#989AF2",
+    purpledark: "#01011D",
+    purplemiddark: "#3D3FBE",
+    purplemidlight: "#B6B6EC",
+    purplelight: "#EFEEFF",
+    orangeprimary: "#FD8C2E",
+    orangedark: "#1C0C00",
+    orangemiddark: "#CF650D",
+    orangemidlight: "#FFA55B",
+    orangelight: "#FFEDDE",
+    yellow: "#F3DA1A",
+    yellowdark: "#171500",
+    yellowmiddark: "#958602",
+    yellowmidlight: "#F4E77E",
+    yellowlight: "#FDFBEC",
+    brown: "#D7B79E",
+    browndark: "#120800",
+    brownmiddark: "#85664D",
+    brownmidlight: "#F4D8C4",
+    brownlight: "#FFF7F0",
+    magenta: "#F35B80",
+    magentadark: "#27000A",
+    magentamiddark: "#B8294B",
+    magentamidlight: "#FF96AF",
+    magentalight: "#FFEFF3",
+    bordercolor: "#BEBEBF",
+    footercolor: "#F3F3F2",
+    caption: "#6E847C",
+    bodycolor: "#4C4C4C",
+  },
+};
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <DefaultSeo {...SEO} />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
-
-export default MyApp
