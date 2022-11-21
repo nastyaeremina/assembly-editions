8f33e3b118588234895c6bc8742582ef35d6543b
krupali-pixer <krupali.nakrani@pixer.digital>
2022-11-21 11:00:19 +0530

    implement module section render dynamically

---
+++ constants/constant.js
@@ -0,0 +1,76 @@
+export const HEADER_LIST = {
+  MESSAGING: 1,
+  FILES: 2,
+  BILLING: 3,
+  FORMS: 4,
+  KNOWLEDGE: 5,
+  CONTRACT: 6,
+};
+
+export const MUDULE_LIST = {
+  BILLING: "billing",
+  CONTRACT: "contract",
+  FILES: "file",
+  FORMS: "form",
+  KNOWLEDGE: "knowledge",
+  MESSAGING: "messege",
+};
+export const NAVBAR_COLOR_LIST = [
+  {
+    bgColor: "#fdfdfb",
+    borderBottomColor: "#CCCCD0",
+    fontColor: "#131313",
+    buttonColor: "#000000",
+    primaryColor: "#09AA6C",
+  },
+  {
+    bgColor: "#120800",
+    borderBottomColor: "#85664D",
+    fontColor: "#F3F3F2",
+    primaryColor: "#D7B79E",
+  },
+  {
+    bgColor: "#01011d",
+    borderBottomColor: "#3D3FBE",
+    fontColor: "#F3F3F2",
+    primaryColor: "#989AF2",
+  },
+  {
+    bgColor: "#01292c",
+    borderBottomColor: "#03888F",
+    fontColor: "#F3F3F2",
+    primaryColor: "#00AFB9",
+  },
+  {
+    bgColor: "#171500",
+    borderBottomColor: "#958602",
+    fontColor: "#F3F3F2",
+    primaryColor: "#F3DA1A",
+  },
+  {
+    bgColor: "#1c0c00",
+    borderBottomColor: "#CF650D",
+    fontColor: "#F3F3F2",
+    primaryColor: "#FD8C2E",
+  },
+  {
+    bgColor: " #27000a",
+    borderBottomColor: "#B8294B",
+    fontColor: "#F3F3F2",
+    primaryColor: "#F35B80",
+  },
+
+  {
+    bgColor: "#fffffd",
+    borderBottomColor: "#CCCCD0",
+    fontColor: "#131313",
+    buttonColor: "#000000",
+    primaryColor: "#09AA6C",
+  },
+];
+export const MODULE_IMAGE_LIST = [
+  {
+    ctaImage: "",
+    quoteBackgroundImage: "",
+  },
+];
