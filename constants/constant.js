1aa8bc20a6cc86a97ce880a704ad5b44d04ab986
maulik-pixer <maulik.balar@pixer.digital>
2022-11-23 17:09:45 +0530

    apps page feedback

--- constants/constant.js
+++ constants/constant.js
@@ -91,3 +91,4 @@ export const MODULE_IMAGE_LIST = [
     quoteBackgroundImage: "",
   },
 ];
+export const MONTH_LIST = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
