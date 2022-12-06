80d287263f5536f790d21921afa03df0aae51933
krupali-pixer <krupali.nakrani@pixer.digital>
2022-12-06 11:13:21 +0530

    set default button props

--- components/button/button.js
+++ components/button/button.js
@@ -1,12 +1,12 @@
 import Link from 'next/link'
-import { React, useEffect, useState } from 'react'
-import { ButtonContainer, Blur } from './style'
+import { React } from 'react'
+import { ButtonContainer } from './style'
 
 function myFunction(e) {
     e.target.style.setProperty("--cursor-x", (e.clientX - e.target.offsetLeft));
     e.target.style.setProperty("--cursor-y", (e.clientY - e.target.offsetTop - document.body.getBoundingClientRect().top));
 }
-export default function Button({ bgColor, hoverColor, borderColor, fontColor, href, text }) {
+export default function Button({ bgColor, hoverColor, borderColor, fontColor, href = "#", text }) {
 
     return (
         <ButtonContainer onMouseMove={(e) => myFunction(e)} backgroundColor={bgColor} hoverColor={hoverColor} borderColor={borderColor} fontColor={fontColor}>
