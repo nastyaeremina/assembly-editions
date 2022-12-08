33a4614497c12e762d175429fd07a99793abb587
Tirth Keraliya <tirth.ketaliya@pixer.io>
2022-12-08 16:17:16 +0530

    [Book demo] - update design for large screen behaviours
    add hover link

--- components/bookdemo/bookDemo.js
+++ components/bookdemo/bookDemo.js
@@ -1,7 +1,18 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import Button from '../button/button';
-import { MainSection, FormSection, FormTxt, FormDetail, Input, NameBlock, NameInfo, LastText, ImgWrap } from './styles';
+import {
+  MainSection,
+  FormSection,
+  FormTxt,
+  FormDetail,
+  Input,
+  NameBlock,
+  NameInfo,
+  LastText,
+  ImgWrap,
+  HelpLink
+} from './styles';
 
 export default function BookDemoForm() {
   return (
@@ -123,7 +134,37 @@ export default function BookDemoForm() {
         </FormDetail>
         <Button text={'Let’s talk'} href={'#'} className='btnposition' />
         <LastText>
-          <p>or start your 14-day free trial</p>
+          <span>or</span>
+          <HelpLink className='icon-link'>
+            <Link href='https://dashboard.copilot.com/onboarding' className='learn-link mb0'>
+              start your 14-day free trial
+              <svg width='16' height='12' viewBox='0 0 16 12' fill='none' class='HoverArrow'>
+                <path
+                  d='M5.7998 1.37109L10.4283 5.99958L5.7998 10.6281'
+                  stroke-width='1.92854'
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  class='HoverArrow__tipPath'
+                />
+                <path
+                  d='M10.33 5.99951H1.5'
+                  stroke-width='2'
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  class='HoverArrow__linePath'
+                />
+              </svg>
+              <svg width='8' height='14' viewBox='0 0 8 14' fill='none' class='mobilearrow'>
+                <path
+                  d='M2 3L6 7L2 11'
+                  stroke='#09AA6C'
+                  stroke-width='1.85714'
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                />
+              </svg>
+            </Link>
+          </HelpLink>
         </LastText>
       </FormSection>
     </MainSection>
