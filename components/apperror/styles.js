9f018cf898cd6cea2b0a5178f45e11d20aa628b9
maulik-pixer <109730293+maulik-pixer@users.noreply.github.com>
2022-12-10 12:59:34 +0530

    [Apps] - search result empty state

---
+++ components/apperror/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { Body1, Body3, Body4, Heading3 } from '../../styles/styles';
+const AppMain = styled.div`
+  text-align: center;
+  margin: auto;
+  max-width: 381px;
+  width: 100%;
+  h4 {
+    ${Body1};
+    color: ${({ theme }) => theme.colors.greendark};
+    margin: 40px 0 0 0;
+  }
+  p {
+    margin: 20px 0 0 0;
+    color: ${({ theme }) => theme.colors.lightgray};
+    ${Body4}
+  }
+  span {
+    font-weight: 500;
+  }
+`;
+export { AppMain };
