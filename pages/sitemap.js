032804e90a7c2f89d55b0fb91a2afcb885696120
krupali-pixer <100576965+krupali-pixer@users.noreply.github.com>
2022-12-26 15:19:14 +0530

    implement render sitemap list dynamic

--- pages/sitemap.js
+++ pages/sitemap.js
@@ -19,30 +19,27 @@ import {
 
 export default function Privacy({ content }) {
   const [sitemap, setSitemap] = useState([])
-  console.log("content", content);
   const loadData = useCallback(() => {
     if (!isEmpty(content)) {
       const list = content?.split('#')
       list?.shift()
-      const contentList = []
+      let sitemapList = []
       list?.forEach(item => {
 
         const newItemList = removeEmptyElement(item?.split("\n"))
-        let newItem = { title: newItemList?.[0], list: [] }
+        const title =newItemList?.[0]
         newItemList?.shift()
         const mapList = []
         newItemList?.forEach(element => {
-          const newObject = ne[1]?.split(/[\[\]\(\)]/)
-          mapList?.push({ name: newObject[1], url: newObject[3] })
+          const newObject = element?.split(/[\[\]\(\)]/)
+          const url= newObject[3]?.split('www.copilot.com')?.[1] || ""
+          mapList?.push({ name: newObject[1], url})
 
         })
-
+        sitemapList?.push(  { title , list: mapList})
+        setSitemap(sitemapList)
       })
-      console.log("list", newList);
-      const ne = removeEmptyElement(newList)
-      console.log("ne", ne);
-      const final = ne[1]?.split(/[\[\]\(\)]/)
-      console.log("final", final);
+     
     }
   }, [content])
 
@@ -53,7 +50,12 @@ export default function Privacy({ content }) {
   const renderSitemapView = useMemo(() => {
     if (isEmpty(sitemap)) return null
     return sitemap?.map((item, index) => {
-      return <></>
+      return <InfoWrap key={`sitemap_index_${index}`}>
+      <h4>{item?.title}</h4>
+      {!isEmpty(item?.list) &&  ( <InfoLink>
+        {item?.list?.map((listItem,listIndex)=>  <Link href={listItem?.url} key={`sitemap_list_index_${listIndex}`}>{listItem?.name}</Link> )}
+      </InfoLink>)}
+    </InfoWrap>
     })
   }, [sitemap])
 
@@ -73,69 +75,7 @@ export default function Privacy({ content }) {
           </PrivacuHero>
           <ContentInfo>
             <Container>
-              <InfoWrap>
-                <h4>Company</h4>
-                <InfoLink>
-                  <Link href='/brands'>Brand</Link>
-                  <Link href='/pricing'>Pricing</Link>
-                  <Link href='/jobs'>Jobs</Link>
-                  <Link href='/copilot-plus'>Copilot Plus</Link>
-                </InfoLink>
-              </InfoWrap>
-              <InfoWrap>
-                <h4>Resources</h4>
-                <InfoLink>
-                  <Link href='http://copilot.com/blog'>Blog</Link>
-                  <Link href='http://security.copilot.com'>Security</Link>
-                  <Link href='http://copilot.com/updates'>What’s New</Link>
-                  <Link href='/university'>Video Tutorials</Link>
-                  <Link href='http://docs.copilot.com/'>API Reference</Link>
-                  <Link href='https://status.copilot.com/'>System Status</Link>
-                </InfoLink>
-              </InfoWrap>
-              <InfoWrap>
-                <h4>Features</h4>
-                <InfoLink>
-                  <Link href='/features/messaging-app'>Messaging App</Link>
-                  <Link href='/features/billing-app'>Billing App</Link>
-                  <Link href='/features/files-app'>Files App</Link>
-                  <Link href='/features/forms-app'>Forms App</Link>
-                  <Link href='/features/helpdesk-app'>Helpdesk App</Link>
-                  <Link href='/apps'>Partner Apps</Link>
-                </InfoLink>
-              </InfoWrap>
-
-              <InfoWrap>
-                <h4>Solutions</h4>
-                <InfoLink>
-                  <Link href='/solutions/accounting-firms'>Accounting Firms</Link>
-                  <Link href='/solutions/marketing-agencies'>Marketing Agencies</Link>
-                  <Link href='/copilot.com/solutions/startups'>Startups</Link>
-                </InfoLink>
-              </InfoWrap>
-              <InfoWrap>
-                <h4>Legal</h4>
-                <InfoLink>
-                  <Link href='/legal/terms-of-service'>Terms of service</Link>
-                  <Link href='/legal/privacy-policy'>Privacy policy</Link>
-                </InfoLink>
-              </InfoWrap>
-              <InfoWrap>
-                <h4>Contact us</h4>
-                <InfoLink>
-                  <Link href='/book-demo'>Book demo</Link>
-                  <Link href='https://join.slack.com/t/copilotcommunity/shared_invite/zt-q6stzfgx-vBf7LBtbKFv63W4n_Otx4w'>
-                    Join community
-                  </Link>
-                </InfoLink>
-              </InfoWrap>
-              <InfoWrap>
-                <h4>Other</h4>
-                <InfoLink>
-                  <Link href='https://dashboard.copilot.com/login?step=signIn'>Log in</Link>
-                  <Link href='https://dashboard.copilot.com/onboarding'>Start trial</Link>
-                </InfoLink>
-              </InfoWrap>
+              {renderSitemapView}
             </Container>
           </ContentInfo>
         </MainSection>
