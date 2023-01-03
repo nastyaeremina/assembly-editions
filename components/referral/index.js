434ebb09b19fc91a3e87b4446c6f36280127a49e
Sanjay Patel <sanjay@pixer.io>
2023-01-03 22:14:36 +0530

    make referral link working

--- components/referral/index.js
+++ components/referral/index.js
@@ -13,7 +13,7 @@ export default function Referral(props) {
               Copilot is used by 1000s of modern services businesses. Get started below with a 14-day free trial. No credit card required.
             </p>
             <PrimaryButton>
-              <a href='#'>Get started</a>
+              <a href={props.url}>Get started</a>
             </PrimaryButton>
           </HeroLeft>
           <HeroRight>
--- pages/referral.js
+++ pages/referral.js
@@ -2,8 +2,20 @@ import { NextSeo } from 'next-seo';
 import Layout from '../components/layout';
 import Navbar from '../components/navbar/navbar';
 import Referral from '../components/referral';
+import { useRouter } from 'next/router'
+import { useState, useEffect } from "react";
 
 export default function ReferralPage() {
+  const router = useRouter();
+  const [code, setCode] = useState((useRouter().query.code || "").toString());
+
+  useEffect(()=>{
+    if(router.query.code != null)
+    {
+      setCode(router.query.code)
+    }
+  },[router]);
+
   return (
     <>
       <NextSeo
@@ -12,7 +24,7 @@ export default function ReferralPage() {
       />
       <Layout>
         <Navbar />
-        <Referral firstName="Marlon"/>
+        <Referral firstName={code.split("_")[0]} url={'https://dashboard.copilot.com/onboarding?referred=' + code.split("_")[1]}/>
       </Layout>
     </>
   );
