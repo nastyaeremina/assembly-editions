eb5a929e5e0a5d6f37d97d31bddc0a11f7baf258
Sanjay Patel <sanjay@pixer.io>
2023-01-07 00:11:16 +0530

    setup google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,10 @@ import { Provider as StyletronProvider } from 'styletron-react';
 import { useCallback, useEffect } from 'react';
 import store from '../store/store';
 import { styletron } from '../styletron';
-
+import Script from "next/script";
 import SEO from '../next-seo.config';
+import { GTM_ID, pageview } from "../lib/gtm";
+import Head from 'next/head'
 import { getSitemap } from '../lib/contentful-sitemap';
 
 const GlobalStyle = createGlobalStyle`
@@ -96,7 +98,6 @@ export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
     loadData();
   }, [loadData]);
-
   return (
     <>
       <Provider store={store}>
