fef424231573392ca0d4bb10f834aafbe019dc26
trupal-pixer <trupal@pixer.digital>
2023-01-09 16:53:45 +0530

    Change hover style of grow business section slider

--- components/businessSlider/businessslider.js
+++ components/businessSlider/businessslider.js
@@ -8,11 +8,11 @@ import { Slide, SliderWrap, WrapImage, LeftBorder, RightBorder, WrapSlide, Slide
 const BusinessSlider = ({ data }) => {
   var settings = {
     speed: 700,
-    autoplay: true,
+    autoplay: false,
     infinite: true,
     autoplaySpeed: 0,
     cssEase: 'linear',
-    slidesToShow: 4.5,
+    slidesToShow: 4.1,
     slidesToScroll: 0.1,
     pauseOnHover: true,
     responsive: [
@@ -55,14 +55,14 @@ const BusinessSlider = ({ data }) => {
     <>
       <SliderWrap>
         <Slider {...settings}>
-          {data?.map((item, index) => {
+          {data?.map((item, index) => { 
             return (
               <>
                 <WrapSlide className='mydiv'>
                   <LeftBorder className='hide'></LeftBorder>
                   <WrapImage>
                     <SlideImg>
-                      <Image src={item?.image?.url} alt='red-icon' className='zoom' width={350} height={350} />
+                      <Image src={`${item?.image?.url}?w=354&h=354&fit=thumb`} alt='red-icon' className='zoom' width={354} height={354} />
                     </SlideImg>
                     <h4>{item?.industry}</h4>
                     <p className='hide'>{item?.quote}</p>
