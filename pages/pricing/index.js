adc54d8a0ef4c250721ae3a19ec5ddf4aa60d544
Allene Norton <norton.allene@gmail.com>
2023-01-12 10:58:30 -0500

    change powered by... to copilot

--- pages/pricing/index.js
+++ pages/pricing/index.js
@@ -420,7 +420,7 @@ export default function NewIndex({ faq, seoData }) {
                       <h4>White labeling</h4>
                       <p>
                         With a custom domain you can host your portal on your own URL. With a custom email domain you
-                        can send client email notifications from your own email. Powered by Portal is a small badge that
+                        can send client email notifications from your own email. Powered by Copilot is a small badge that
                         shows in the client experience and can be removed on the Advanced plan.
                       </p>
                     </td>
