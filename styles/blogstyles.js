a3df167ce6c6df9e2d2b5f61ca6a88346fcd494b
trupal-pixer <trupal@pixer.digital>
2023-01-27 18:27:41 +0530

    sharing button working in blog details page

--- styles/blogstyles.js
+++ styles/blogstyles.js
@@ -361,8 +361,7 @@ const Icon = styled.div`
     display:flex;
     justify-content:space-between;
     div{
-        max-width:36px;
-        width:100%;
+        width:36px;
         height:36px;
         display:flex;
         align-items:center;
@@ -374,8 +373,7 @@ const Icon = styled.div`
             background-color:#09AA6C;
         }
          @media only screen and (max-width:426px){
-            max-width:30px;
-            width:100%;
+            width:30px;
             height:30px;
          }
     }
