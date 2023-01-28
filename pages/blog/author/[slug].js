c2dca3f815d73c403b48aaeb77477553d06605b7
krupali-pixer <100576965+krupali-pixer@users.noreply.github.com>
2023-01-28 14:11:06 +0530

    set author and tag seo meta data

--- pages/blog/author/[slug].js
+++ pages/blog/author/[slug].js
@@ -1,13 +1,14 @@
 import { useMemo } from 'react';
 import moment from 'moment';
+import { NextSeo } from 'next-seo';
 import Layout from '../../../components/layout';
 import BlogNavbar from '../../../components/navbar/blognavbar';
 import { Container } from '../../../styles/commonStyles';
 import Blogcard from '../../../components/Blogcard';
-import { getAllAuthorWithSlug, getAllTagWithSlug, getBlogByAuthor } from '../../../lib/blog-content';
+import { getAllAuthorWithSlug, getAllTagWithSlug, getAuthorDetail, getBlogByAuthor } from '../../../lib/blog-content';
 import { isEmpty } from '../../../helpers/helpers';
 
-export default function Author({ seoData, allPosts, tags }) {
+export default function Author({ allPosts, tags, authorDetail }) {
   const renderData = useMemo(() => {
     if (isEmpty(allPosts)) return null;
     return allPosts?.map((item, index) => {
@@ -32,8 +33,46 @@ export default function Author({ seoData, allPosts, tags }) {
   const renderNavbar = useMemo(() => {
     return <BlogNavbar tagData={tags} />;
   }, [tags]);
+
+  const renderSeoData = useMemo(() => {
+    const title = `${authorDetail?.name} - Copilot Blog`;
+    const og_title = authorDetail?.og_title ?? authorDetail?.meta_title ?? title;
+    const og_des = authorDetail?.meta_description ?? authorDetail?.description;
+    const og_image = authorDetail?.feature_image;
+
+    return (
+      <NextSeo
+        title={authorDetail?.meta_title ?? title}
+        description={authorDetail?.meta_description ?? authorDetail?.description}
+        openGraph={{
+          type: 'website',
+          locale: 'en_IE',
+          site_name: 'copilot.com',
+          title: { og_title },
+          description: { og_des },
+          images: isEmpty(authorDetail?.og_image)
+            ? []
+            : [
+                {
+                  url: og_image
+                }
+              ]
+        }}
+      />
+    );
+  }, [
+    authorDetail?.description,
+    authorDetail?.feature_image,
+    authorDetail?.meta_description,
+    authorDetail?.meta_title,
+    authorDetail?.name,
+    authorDetail?.og_image,
+    authorDetail?.og_title
+  ]);
+
   return (
     <>
+      {renderSeoData}
       {renderNavbar}
       <Layout>
         <Container>{renderData}</Container>
@@ -42,16 +81,16 @@ export default function Author({ seoData, allPosts, tags }) {
   );
 }
 export async function getStaticProps({ params, preview = false }) {
-  // const seoData = (await getSEOdata(BOOK_DEMO_SEO_ID)) ?? [];
-  const seoData = [];
   const allPosts = (await getBlogByAuthor(params?.slug)) ?? [];
+  const authorDetail = (await getAuthorDetail(params?.slug)) ?? {};
+
   const tags = (await getAllTagWithSlug()) ?? [];
   const finalTagList = tags?.filter((tag) => tag?.name?.trim()?.[0] !== '#');
   return {
     props: {
-      seoData,
       allPosts,
-      tags: finalTagList
+      tags: finalTagList,
+      authorDetail
     }
   };
 }
