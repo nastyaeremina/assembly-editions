b16ea669b0d4e1fc101c3ada9a29ee438109e035
dev-pixer <krupali.nakrani@pixer.digital>
2023-02-27 17:28:59 +0530

    Revert "fix display string null issue"
    This reverts commit 50f9342faf4be0aa14dc0db3b99445aa7f114de7.

--- pages/comparison/[slug].js
+++ pages/comparison/[slug].js
@@ -123,11 +123,11 @@ export default function Comparison({ postDetail: details, seoData }) {
         return (
           <Details key={`g2comparisonrow_${index}`}>
             <Image
-              src={isEmpty(value) || value?.toLowerCase() === 'no' ? cancelmark : checkmark}
+              src={value?.toLowerCase() === 'limited' || value?.toLowerCase() === 'no' ? cancelmark : checkmark}
               alt='check-mark'
               className='mobilecheckmark'
             />
-            <p>{isUpdateTilte && !isEmpty(value) ? `${value} ${item?.name}` : item?.name}</p>
+            <p>{isUpdateTilte ? `${value} ${item?.name}` : item?.name}</p>
           </Details>
         );
       });
