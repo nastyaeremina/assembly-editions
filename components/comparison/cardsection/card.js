c2b902c42bd5d76e25a64f3be4c83e354f6cb46f
dev-pixer <krupali.nakrani@pixer.digital>
2023-03-03 16:31:05 +0530

    implement comparison page functional

--- components/comparison/cardsection/card.js
+++ components/comparison/cardsection/card.js
@@ -2,10 +2,10 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Carditem, CardLogo, ComparisonLogo } from '../styles';
 
-export default function Card({ src }) {
+export default function Card({ src, slug }) {
   return (
     <>
-      <Link href='/comparison/copilot-vs-suitedash'>
+      <Link href={`/comparison/${slug}`}>
         <Carditem>
           <CardLogo>
             <Image src='/images/logo.svg' alt='logo' width={231} height={50} layout={'fixed'} className='mobilecard' />
--- components/comparison/cardsection/cardsection.js
+++ components/comparison/cardsection/cardsection.js
@@ -1,14 +1,23 @@
+import { useMemo } from 'react';
+import { isEmpty } from '../../../helpers/helpers';
 import { Allcard } from '../styles';
 import Card from './card';
 
-export default function Cardsection() {
+export default function Cardsection({ data }) {
+  const renderCardList = useMemo(() => {
+    if (isEmpty(data)) return null;
+    return data?.map((item, index) => (
+      <Card src={item?.logo?.url} slug={item?.slug} key={`competitor_index_${index}`} />
+    ));
+  }, [data]);
   return (
     <>
       <Allcard>
+        {renderCardList}
+        {/* <Card src={'/images/comparisonlogo.svg'} />
         <Card src={'/images/comparisonlogo.svg'} />
         <Card src={'/images/comparisonlogo.svg'} />
-        <Card src={'/images/comparisonlogo.svg'} />
-        <Card src={'/images/comparisonlogo.svg'} />
+        <Card src={'/images/comparisonlogo.svg'} /> */}
       </Allcard>
     </>
   );
