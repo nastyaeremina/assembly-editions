cc2c0dffba8000ba5b340a5ae03ea9ba328573fb
dev-pixer <krupali.nakrani@pixer.digital>
2023-03-11 14:55:41 +0530

    make cta content dynamic

--- actions/appActions.js
+++ actions/appActions.js
@@ -9,3 +9,7 @@ export const setAppLoading = createAction(Actions.APP_SET_LOADING);
  * @desc Set Top bar content
  */
 export const setTopBarContent = createAction(Actions.SET_TOP_BAR_CONTENT);
+/**
+ * @desc Set CTA content
+ */
+export const setCTAContent = createAction(Actions.SET_CTA_CONTENT);
