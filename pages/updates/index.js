907fdb62eab851b0dcc2b8cfb1a9524e5aaddc3d
dev-pixer <krupali.nakrani@pixer.digital>
2023-03-16 16:23:06 +0530

    implement pagination working

--- pages/updates/index.js
+++ pages/updates/index.js
@@ -67,17 +67,8 @@ export default function Updates({ seoData, allPosts }) {
                 bgColor={'transparent'}
                 fontColor={'#000000'}
                 borderColor={'#000000'}
-                text={'Previous page'}
-                href={'#'}
-                hoverColor={'rgba(0, 0, 0, 0.5)'}
-                className={'pagination-button'}
-              />
-              <Button
-                bgColor={'transparent'}
-                fontColor={'#000000'}
-                borderColor={'#000000'}
                 text={'Next Page'}
-                href={'#'}
+                href={`/updates/page/2`}
                 hoverColor={'rgba(0, 0, 0, 0.5)'}
                 className={'pagination-button'}
               />
@@ -91,7 +82,7 @@ export default function Updates({ seoData, allPosts }) {
 }
 export async function getStaticProps({ preview = false }) {
   const seoData = (await getSEOdata(UPDATES_SEO_ID)) ?? [];
-  const allPosts = (await getUpdatesPosts()) ?? [];
+  const allPosts = (await getUpdatesPosts({ page: 1 })) ?? [];
   seoData.canonical = 'https://www.copilot.com/updates';
 
   return {
