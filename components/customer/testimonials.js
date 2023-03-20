cf4c3de0bf410680e2464bc9bbed7f1cfc365127
dev-pixer <krupali.nakrani@pixer.digital>
2023-03-20 18:30:12 +0530

    implement customer page render dynamic

--- components/customer/testimonials.js
+++ components/customer/testimonials.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Image from 'next/image';
 import { Container } from '../../styles/commonStyles';
 import rightphoto from '../../public/images/price.png';
+import { convertHighlights, isEmpty } from '../../helpers/helpers';
 import { Detail, Last, LastDroplist, LeftCard, Percentage, RightCard, Section, TestimonialCard, Top } from './styles';
 
-export default function CustomerTestimonial({ logo, body, satisfaction, rate, retention }) {
+export default function CustomerTestimonial({ logo, body, slug, highlightsData }) {
+  const renderHighlightView = useMemo(() => {
+    const newList = convertHighlights(highlightsData);
+
+    if (isEmpty(newList)) return null;
+    return (
+      <Percentage>
+        {newList?.map((item, index) => {
+          return (
+            <Section key={`highlight_index_${index}`}>
+              <span>{item?.title}</span>
+              <p>{item?.desc}</p>
+            </Section>
+          );
+        })}
+      </Percentage>
+    );
+  }, [highlightsData]);
+
   return (
     <Container>
       <TestimonialCard>
@@ -12,24 +31,11 @@ export default function CustomerTestimonial({ logo, body, satisfaction, rate, re
           <Top>
             <Image src={logo} alt='customer' width={218} height={50} className='top-logo' />
             <Detail>{body}</Detail>
-            <Percentage>
-              <Section>
-                <span>{satisfaction}</span>
-                <p>Customer satisfaction</p>
-              </Section>
-              <Section>
-                <span>{rate}</span>
-                <p>Customer Churn rate</p>
-              </Section>
-              <Section>
-                <span>{retention}</span>
-                <p>Retention</p>
-              </Section>
-            </Percentage>
+            {!isEmpty(highlightsData) && renderHighlightView}
           </Top>
           <LastDroplist>
             <Last className='icon-link'>
-              <a href={'#'} className='learn-link mb0'>
+              <a href={`case-study/${slug}`} className='learn-link mb0'>
                 View full case study
                 <svg width='16' height='12' viewBox='0 0 16 12' fill='none' class='HoverArrow'>
                   <path
