7a735ae7c279249808e77e1338723d356548a317
trupal-pixer <trupal@pixer.digital>
2023-03-23 15:46:03 +0530

    update page padding-top

--- components/Home/styles.js
+++ components/Home/styles.js
@@ -3,7 +3,7 @@ import { Body1, Body2, Body5, FooterText, Heading2, Heading3, MbBody1, MobileH2 
 
 const HeroSection = styled.div`
   width: 100%;
-  padding: 224px 0 0px 0;
+  padding: 180px 0 0px 0;
   /* margin-bottom: 50px; */
   text-align: center;
   overflow: hidden;
--- styles/casestudiestyles.js
+++ styles/casestudiestyles.js
@@ -3,7 +3,7 @@ import { Body2, Body3, Body4, Body5, HeaderFont, Heading2, Heading3, Heading4, H
 
 const HeroSection = styled.div`
   width: 100%;
-  padding: 224px 0 0px 0;
+  padding: 180px 0 0px 0;
   text-align: center;
   overflow: hidden;
   @media only screen and (max-width: 749px) {
