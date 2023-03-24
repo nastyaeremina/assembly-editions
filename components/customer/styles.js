38d15a5c821e822f38155b0a86137ace87b081ed
trupal-pixer <trupal@pixer.digital>
2023-03-24 12:27:11 +0530

    fix banner size issue

--- components/customer/styles.js
+++ components/customer/styles.js
@@ -23,11 +23,12 @@ const RightCard = styled.div`
     max-width: 100%;
   }
   .right {
-    width: 405px;
+    max-width: 405px;
+    width:100%;
     border-left: 1px solid #131313;
     border-radius: 0px 3px 3px 0px;
     @media only screen and (max-width: 768px) {
-        width:100%;
+        max-width:100%;
         border-left:none;
         border-radius:3px 3px 0px 0px;
     }
