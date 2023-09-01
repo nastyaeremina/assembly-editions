55240f5c6f56523d0d41d65182bb2fa8c26e4300
Sanjay Patel <makasana.dev@gmail.com>
2023-09-01 23:17:23 +0530

    fix revalidation logic with tags

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -1,8 +1,9 @@
 import { NextResponse } from 'next/server'
-import { revalidatePath } from 'next/cache'
- 
+import { revalidateTag } from 'next/cache'
+
 export async function GET(request) {
   const path = request.nextUrl.searchParams.get('path') || '/'
-  revalidatePath(path)
+  revalidateTag('guide')
+  revalidateTag('other')
   return NextResponse.json({ revalidated: true, now: Date.now() })
 }
