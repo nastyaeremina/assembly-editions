6b5898f5c8dcd469276aa2d648554d44c96fc2c6
krupali-pixer <100576965+krupali-pixer@users.noreply.github.com>
2025-08-04 17:09:38 +0530

    remove unused variable (#904)

--- app/constants/externalLinks.js
+++ app/constants/externalLinks.js
@@ -11,7 +11,6 @@ export const COPILOT_REFERENCE_API_LINK = 'https://docs.copilot.app/reference/';
 export const COPILOT_JOIN_COMMUNITY_LINK =
   'https://join.slack.com/t/copilotcommunity/shared_invite/zt-1n4u6dr8m-Bm_8Jnmq6_RoY1ySq~yVcQ';
 export const COPILOT_SECURITY_LINK = 'https://security.copilot.app/';
-export const COPILOT_UPDATE_LINK = 'https://copilot.app/updates';
 export const UPDATE_SUBSCRIBE_LINK = 'https://x.com/intent/user?screen_name=copilotplatform';
 export const CUSTOM_APP_LINK = 'https://docs.copilot.app/docs/custom-apps-overview';
 export const COPILOT_COMMUNITY_LINK = 'https://community.copilot.app/';
