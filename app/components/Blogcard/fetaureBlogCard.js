17bf7884d6041a195842392e0d37ea6dced16935
krupali-pixer <100576965+krupali-pixer@users.noreply.github.com>
2025-08-22 22:51:39 +0530

    used next link for navigate blog detail page instead of router (#911)

--- app/components/Blogcard/fetaureBlogCard.js
+++ app/components/Blogcard/fetaureBlogCard.js
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import moment from 'moment';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 import { BlogCardDiv, FirstBlog, IconDiv, Last, Par, PostDetail, Text, Top } from '../../styles/blogstyles';
 import { isEmpty } from '../../helpers/helpers';
 import { Container } from '../../styles/commonStyles';
@@ -11,51 +11,52 @@ import SVGComponent from '../../../public/images/svg/SVGComponent';
 import { DetailLeftDiv } from './styles';
 
 export default function FeatureBlogCard({ slug, title, excerpt, featureImage, publishedAt, authorName, tags = [] }) {
-  const router = useRouter();
   const [imageHeight, setImageHeight] = useState();
 
   return (
-    <FirstBlog onClick={() => router.push(`/blog/${slug}`)}>
-      <Container>
-        <h1>Blog</h1>
-        <BlogCardDiv>
-          {!isEmpty(featureImage) && (
-            <Top maxHeight={imageHeight}>
-              <Image
-                src={featureImage}
-                className='image'
-                alt='blog'
-                width={720}
-                height={354}
-                priority
-                onLoad={(e) => setImageHeight(e.target.offsetHeight)}
-              />
-            </Top>
-          )}
-          <Text>
-            <PostDetail>
-              <DetailLeftDiv>
-                {!isEmpty(publishedAt) && moment(new Date(publishedAt)).format('MMM DD, YYYY')}
-                {!isEmpty(publishedAt) && (
-                  <IconDiv>
-                    <SVGComponent name='dot-icon' width='6' height='6' viewBox='0 0 6 6' />
-                  </IconDiv>
-                )}
-                {!isEmpty(authorName) && <li>{authorName}</li>}
-              </DetailLeftDiv>
-            </PostDetail>
-            <h3>{title}</h3>
-            {!isEmpty(excerpt) && <Par>{excerpt}</Par>}
-          </Text>
-          {!isEmpty(tags) && (
-            <Last>
-              {tags.map((tag) => (
-                <p key={`tag_${tag.name}`}>{tag.name}</p>
-              ))}
-            </Last>
-          )}
-        </BlogCardDiv>
-      </Container>
-    </FirstBlog>
+    <Link href={`/blog/${slug}`}>
+      <FirstBlog>
+        <Container>
+          <h1>Blog</h1>
+          <BlogCardDiv>
+            {!isEmpty(featureImage) && (
+              <Top maxHeight={imageHeight}>
+                <Image
+                  src={featureImage}
+                  className='image'
+                  alt='blog'
+                  width={720}
+                  height={354}
+                  priority
+                  onLoad={(e) => setImageHeight(e.target.offsetHeight)}
+                />
+              </Top>
+            )}
+            <Text>
+              <PostDetail>
+                <DetailLeftDiv>
+                  {!isEmpty(publishedAt) && moment(new Date(publishedAt)).format('MMM DD, YYYY')}
+                  {!isEmpty(publishedAt) && (
+                    <IconDiv>
+                      <SVGComponent name='dot-icon' width='6' height='6' viewBox='0 0 6 6' />
+                    </IconDiv>
+                  )}
+                  {!isEmpty(authorName) && <li>{authorName}</li>}
+                </DetailLeftDiv>
+              </PostDetail>
+              <h3>{title}</h3>
+              {!isEmpty(excerpt) && <Par>{excerpt}</Par>}
+            </Text>
+            {!isEmpty(tags) && (
+              <Last>
+                {tags.map((tag) => (
+                  <p key={`tag_${tag.name}`}>{tag.name}</p>
+                ))}
+              </Last>
+            )}
+          </BlogCardDiv>
+        </Container>
+      </FirstBlog>
+    </Link>
   );
 }
--- app/components/Blogcard/index.js
+++ app/components/Blogcard/index.js
@@ -2,15 +2,13 @@
 
 import Image from 'next/image';
 import { useMemo } from 'react';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 import { IconDiv, Par, PostDetail, Textarea } from '../../styles/blogstyles';
 import { isEmpty } from '../../helpers/helpers';
 import SVGComponent from '../../../public/images/svg/SVGComponent';
 import { BlogDetail, Bottom, DetailLeftDiv, Leftside } from './styles';
 
 export default function Blogcard(props) {
-  const router = useRouter();
-
   const tagListReder = useMemo(() => {
     if (isEmpty(props.tags)) return null;
     return props.tags?.map((item, index) => {
@@ -22,30 +20,38 @@ export default function Blogcard(props) {
 
   return (
     <>
-      <BlogDetail onClick={() => router.push(`/blog/${props.slug}`)}>
-        {!isEmpty(props.image) && (
-          <Leftside>
-            <Image src={props.image} width={266} height={266} className='image' alt='blog' />
-          </Leftside>
-        )}
-        <Textarea>
-          <PostDetail>
-            <DetailLeftDiv>
-              {!isEmpty(props.date) && props.date}
-              {!isEmpty(props.date) && !isEmpty(props.authorName) && (
-                <IconDiv>
-                  <SVGComponent name='dot-icon' width='6' height='6' viewBox='0 0 6 6' />
-                </IconDiv>
-              )}
-              {!isEmpty(props.authorName) && <li>{`${props.authorName}`}</li>}
-            </DetailLeftDiv>
-            <SVGComponent name='blog-card-hover-arrow-icon' width='16' height='16' viewBox='0 0 16 16' className='svg-icon'/>
-          </PostDetail>
-          <h3>{props.name}</h3>
-          <Par>{props.desc}</Par>
-        </Textarea>
-        {!isEmpty(props.tags) && <Bottom>{tagListReder}</Bottom>}
-      </BlogDetail>
+      <Link href={`/blog/${props.slug}`}>
+        <BlogDetail>
+          {!isEmpty(props.image) && (
+            <Leftside>
+              <Image src={props.image} width={266} height={266} className='image' alt='blog' />
+            </Leftside>
+          )}
+          <Textarea>
+            <PostDetail>
+              <DetailLeftDiv>
+                {!isEmpty(props.date) && props.date}
+                {!isEmpty(props.date) && !isEmpty(props.authorName) && (
+                  <IconDiv>
+                    <SVGComponent name='dot-icon' width='6' height='6' viewBox='0 0 6 6' />
+                  </IconDiv>
+                )}
+                {!isEmpty(props.authorName) && <li>{`${props.authorName}`}</li>}
+              </DetailLeftDiv>
+              <SVGComponent
+                name='blog-card-hover-arrow-icon'
+                width='16'
+                height='16'
+                viewBox='0 0 16 16'
+                className='svg-icon'
+              />
+            </PostDetail>
+            <h3>{props.name}</h3>
+            <Par>{props.desc}</Par>
+          </Textarea>
+          {!isEmpty(props.tags) && <Bottom>{tagListReder}</Bottom>}
+        </BlogDetail>
+      </Link>
     </>
   );
 }
