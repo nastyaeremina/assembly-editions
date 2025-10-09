337a32672864f835d5809187bf8cdc8236339c41
pinal-pixer <133858135+pinal-pixer@users.noreply.github.com>
2025-10-09 09:54:28 +0530

    Added page wrapper for all page (#1032)

--- app/components/layout.js
+++ app/components/layout.js
@@ -1,20 +1,20 @@
 import { isEmpty } from '../helpers/helpers';
 import Analytics from './analytics/analytics';
-import MainFooter from '../components/footer/mainFooter'; 
+import MainFooter from '../components/footer/mainFooter';
 import Navbar from './navbar/navbar';
 
 /**
  * Layout Component
- * 
+ *
  * Main layout wrapper that provides the page structure including footer and analytics.
- * 
+ *
  * @param {Object} props - Component props
  * @param {React.ReactNode} props.children - Child components to render
  * @param {boolean} [props.isGlossary=false] - Whether this is a glossary page
  * @param {string} [props.abTestContentLabel=''] - AB test content label for analytics
  * @param {string} [props.abTestExperimentName=''] - AB test experiment name for analytics
  * @returns {Promise<JSX.Element>} - Promise that resolves to the rendered layout
- * 
+ *
  * @example
  * <Layout isGlossary={false} abTestContentLabel="variant-a" abTestExperimentName="home-test">
  *   <PageContent />
@@ -27,7 +27,7 @@ export default async function Layout({
   abTestExperimentName = ''
 }) {
   return (
-    <>
+    <div className='page-wrapper'>
       <Navbar />
       <div>
         <main>{children}</main>
@@ -36,6 +36,6 @@ export default async function Layout({
       {!isEmpty(abTestExperimentName) && !isEmpty(abTestContentLabel) && (
         <Analytics experimentName={abTestExperimentName} contentLabel={abTestContentLabel} />
       )}
-    </>
+    </div>
   );
 }
