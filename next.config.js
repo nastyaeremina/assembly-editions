8f1b293233ca38e70fe616be257537a6d676f76f
pinal-pixer <133858135+pinal-pixer@users.noreply.github.com>
2026-02-13 12:01:38 +0530

    Added content security policy (#1097)

--- next.config.js
+++ next.config.js
@@ -134,6 +134,31 @@ const nextConfig = {
         }
       ]
     };
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'none'"
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          }
+        ]
+      }
+    ];
   }
 };
 
